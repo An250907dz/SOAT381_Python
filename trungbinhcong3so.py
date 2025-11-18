db3edc1ef075e48d6e4eb3b21a39be6a5f079048
An250907dz <nguyenbinhan2509@gmail.com>
2025-11-18 15:35:36 +0700

    Create trungbinhcong3so.py

---
+++ trungbinhcong3so.py
@@ -0,0 +1,6 @@
+print("=== TRUNG BÌNH CỘNG CỦA 3 SỐ ===")
+a = float(input("Nhập số thứ nhất: "))
+b = float(input("Nhập số thứ hai: "))
+c = float(input("Nhập số thứ ba: "))
+trung_binh = (a + b + c) / 3
+print(f"Trung bình cộng: {trung_binh:.2f}")
